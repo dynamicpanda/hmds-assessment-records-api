6bc90efbed5832320832ec8508df0759c383e217
Nathan Tasker <nmtask@gmail.com>
2021-12-31 13:23:20 -0500

    added documentation and formalized test

--- src/main/java/com/hmds/assessment/WebService.java
+++ src/main/java/com/hmds/assessment/WebService.java
@@ -13,24 +13,47 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Spring Boot REST controller for the HMDS message API. Exposes endpoints for
+ * accessing various types of messages.
+ */
 @RestController
 public class WebService {
+
+  /**
+   * Exposes GET request to retrieve a message based on a message type provided
+   * in the URI. Messages are predefined in the messages.properties resource
+   * file.
+   * 
+   * @param type The type of message to retrieve. Provided in the URI path.
+   * @return The message associated with the provided message type.
+   * @throws IOException Raised if there is a problem reading the properties
+   * file.
+   */
   @GetMapping("/message/{type}")
   @ResponseBody
   public ResponseEntity<String> getMessage(@PathVariable String type) throws IOException {    
-    System.out.println(type);
 
+    // read the messages from the messages.properties file
     Properties properties = new Properties();
     properties.load(WebService.class.getClassLoader().getResourceAsStream("messages.properties"));
 
-    String message = properties.getProperty("messages." + type);
+    // retrieve the message associated with the type. Will be {@code null} if
+    // type is invalid
+    String message = properties.getProperty(type);
+
+    // if valid type, return a JSON object containing the type and value of the
+    // message
     if (message != null) {
       JsonObject json = Json.createObjectBuilder()
-        .add("message", message)
+        .add("type", type)
+        .add("value", message)
         .build();
       return new ResponseEntity<String>(json.toString(), HttpStatus.OK);
     }
-    else
+    // if type invalid, return 400
+    else {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    }
   }
 }
--- src/test/java/com/hmds/assessment/WebServiceTest.java
+++ src/test/java/com/hmds/assessment/WebServiceTest.java
@@ -3,11 +3,13 @@
  */
 package com.hmds.assessment;
 
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Properties;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,20 +19,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-
+/**
+ * Test class for HMDS message API framework.
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class WebServiceTest {
 
+	// the mock model/view/controller with which to run the web service
 	@Autowired
 	private MockMvc mockMvc;
 
+	/**
+	 * Test that a greeting message properly returns the correct greeting.
+	 * @throws Exception
+	 */
 	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
+	public void shouldReturnGreetingMessage() throws Exception {
+
+		final String TYPE = "greeting";
+
+		// retrieve the expected message from the properties file
+		Properties messages = new Properties();
+    	messages.load(WebService.class.getClassLoader().getResourceAsStream("messages.properties"));
+		String expectedMessage = messages.getProperty(TYPE);
+
+		// call the service and assert that it responds ok with the correct
+		// message
 		this.mockMvc.perform(get("/message/greeting"))
             .andDo(print())
             .andExpect(status().isOk())
-			.andExpect(content().string(containsString("Hello, World!")));
+			.andExpect(jsonPath("$.type", is(TYPE)))
+			.andExpect(jsonPath("$.value", is(expectedMessage)));
 	}
 }
